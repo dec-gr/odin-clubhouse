4c36d14b6dd6f6bafc7d4e0d2531b40b305015b0
Declan <144496393+dec-gr@users.noreply.github.com>
2024-12-31 19:02:24 +0000

    Tidy up flow between routes

--- controllers/clubhouseController.js
+++ controllers/clubhouseController.js
@@ -3,48 +3,62 @@ const db = require('../db/queries');
 const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
+/* -------- PUBLIC ROUTES ---------- */
+
 exports.getIndex = (req, res, next) => {
+  if (req.user) {
+    res.redirect('/messageFeed');
+  }
+
   res.render('index', { title: 'Index' });
 };
 
 exports.getRegister = async (req, res, next) => {
+  if (req.user) {
+    res.redirect('/messageFeed');
+  }
+
   res.render('register', {
     title: 'Register',
   });
 };
 
 exports.postRegister = async (req, res, next) => {
+  if (req.user) {
+    res.redirect('/messageFeed');
+  }
+
   const { first_name, last_name, email, password } = req.body;
   try {
     bcrypt.hash(password, 10, async (err, hashedPassword) => {
       await db.addUser({ first_name, last_name, email, hashedPassword });
     });
-    res.redirect('/');
+    res.redirect('/login');
   } catch (err) {
     return next(err);
   }
 };
 
 exports.getLogin = async (req, res, next) => {
-  const form =
-    '<h1>Login Page</h1><form method="post" action="login">\
-                  Enter Username:<br><input type="text" name="email">\
-                  <br>Enter Password:<br><input type="password" name="password">\
-                  <br><br><input type="submit" value="Submit"></form>';
+  if (req.user) {
+    res.redirect('/messageFeed');
+  }
 
-  res.send(form);
+  res.render('login', { title: 'Log In' });
 };
 
 exports.postLogin = async (req, res, next) => {
+  if (req.user) {
+    res.redirect('/messageFeed');
+  }
+
   passport.authenticate('local', {
-    failureRedirect: '/login-failure',
-    successRedirect: '/login-success',
+    failureRedirect: '/',
+    successRedirect: '/messageFeed',
   })(req, res, next);
 };
 
-exports.getHome = (req, res, next) => {
-  res.send('This is the home page');
-};
+/* -------------- USER ROUTES ------------ */
 
 exports.getLogout = (req, res, next) => {
   req.logout((err) => {
@@ -54,8 +68,6 @@ exports.getLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
-
-/* -----  MEMBERSHIP FUNCTIONS ------ */
 
 exports.getAddMember = (req, res, next) => {
   res.render('addMember', { title: '' });
@@ -68,24 +80,8 @@ exports.postAddMember = (req, res, next) => {
     db.addMember(req.user.user_id);
   }
 
-  res.render('index', { title: 'Home' });
+  res.redirect('/messageFeed');
 };
-
-exports.getAddAdmin = (req, res, next) => {
-  res.render('addAdmin', { title: '' });
-};
-
-exports.postAddAdmin = (req, res, next) => {
-  const { secret_code } = req.body;
-
-  if (secret_code === process.env.ADMIN_CODE) {
-    db.addAdmin(req.user.user_id);
-  }
-
-  res.render('index', { title: 'Home' });
-};
-
-/* ----- MESSAGE FUNCTIONS ---- */
 
 exports.getNewMessage = (req, res, next) => {
   res.render('newMessage', {
@@ -104,12 +100,6 @@ exports.postNewMessage = async (req, res, next) => {
   }
 };
 
-exports.getDeleteMessage = async (req, res, next) => {
-  const message_id = req.params.message_id;
-  await db.deleteMessage(message_id);
-  res.redirect('/messageFeed');
-};
-
 exports.getMessageFeed = async (req, res, next) => {
   const queryFunction = req.user.is_member
     ? db.getAllMessagesWithNamesMember
@@ -117,4 +107,28 @@ exports.getMessageFeed = async (req, res, next) => {
   const messages = await queryFunction();
 
   res.render('messageFeed', { title: 'Message Feed', messages: messages });
+};
+
+/* -----  MEMBER ROUTES ------ */
+
+exports.getAddAdmin = (req, res, next) => {
+  res.render('addAdmin', { title: '' });
+};
+
+exports.postAddAdmin = (req, res, next) => {
+  const { secret_code } = req.body;
+
+  if (secret_code === process.env.ADMIN_CODE) {
+    db.addAdmin(req.user.user_id);
+  }
+
+  res.redirect('/messageFeed');
+};
+
+/* ----- ADMIN FUNCTIONS ---- */
+
+exports.getDeleteMessage = async (req, res, next) => {
+  const message_id = req.params.message_id;
+  await db.deleteMessage(message_id);
+  res.redirect('/messageFeed');
 };
--- routes/clubhouseRouter.js
+++ routes/clubhouseRouter.js
@@ -7,6 +7,8 @@ const isAdmin = require('./authMiddleware').isAdmin;
 const clubhouseController = require('../controllers/clubhouseController');
 const clubhouseRouter = Router();
 
+/* -------- PUBLIC ROUTES ------ */
+
 clubhouseRouter.get('/', clubhouseController.getIndex);
 
 clubhouseRouter.get('/register', clubhouseController.getRegister);
@@ -15,11 +17,16 @@ clubhouseRouter.post('/register', clubhouseController.postRegister);
 clubhouseRouter.get('/login', clubhouseController.getLogin);
 clubhouseRouter.post('/login', clubhouseController.postLogin);
 
+/* -------- USER ROUTES ------- */
+
 clubhouseRouter.get('/logout', isAuth, clubhouseController.getLogout);
 
-clubhouseRouter.get('/home', clubhouseController.getHome);
-
 clubhouseRouter.get('/messageFeed', isAuth, clubhouseController.getMessageFeed);
+
+clubhouseRouter.get('/addMember', isAuth, clubhouseController.getAddMember);
+clubhouseRouter.post('/addMember', isAuth, clubhouseController.postAddMember);
+
+/* -------- MEMBER ROUTES ------- */
 
 clubhouseRouter.get('/newMessage', isMember, clubhouseController.getNewMessage);
 clubhouseRouter.post(
@@ -28,16 +35,15 @@ clubhouseRouter.post(
   clubhouseController.postNewMessage
 );
 
+clubhouseRouter.get('/addAdmin', isMember, clubhouseController.getAddAdmin);
+clubhouseRouter.post('/addAdmin', isMember, clubhouseController.postAddAdmin);
+
+/* -------- ADMIN ROUTES ------- */
+
 clubhouseRouter.get(
   '/messages/:message_id/delete',
   isAdmin,
   clubhouseController.getDeleteMessage
 );
-
-clubhouseRouter.get('/addMember', isAuth, clubhouseController.getAddMember);
-clubhouseRouter.post('/addMember', isAuth, clubhouseController.postAddMember);
-
-clubhouseRouter.get('/addAdmin', isAuth, clubhouseController.getAddAdmin);
-clubhouseRouter.post('/addAdmin', isAuth, clubhouseController.postAddAdmin);
 
 module.exports = clubhouseRouter;
